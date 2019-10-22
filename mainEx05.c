07a9ab7de933000602181f1dad15e777cf48fc3a
Bernardo de Oliveira Bruning <bernardo.bruning@gmail.com>
2019-10-22 00:41:14 +0000

    implementa loop 10x

--- mainEx05.c
+++ mainEx05.c
@@ -8,13 +8,14 @@
 
 double speedup(double tempo_sequencial, double tempo_paralelo);
 void imprime_speedup(char* descricao, double tempo_sequencial, double tempo_paralelo);
+float soma_tempo(float tempo[10]);
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 int main(int argc, char *argv[]) {
 
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
   // DECLARAÇÃO de VARIÁVEIS
-  float tempos[4];
+  float tempos[4][10];
   mymatriz mat_a, mat_b;
   mymatriz **mmultbloco, **mmult;
   char filename[100];
@@ -55,7 +56,7 @@ int main(int argc, char *argv[]) {
   free (vet_line);
   fclose(fmat);
   printf("\n Matriz A.\n");
-  mimprimir(&mat_a);
+  //mimprimir(&mat_a);
   // %%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%
 
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
@@ -76,91 +77,99 @@ int main(int argc, char *argv[]) {
   free (vet_line);
   fclose(fmat);
   printf("\n Matriz B\n");
-  mimprimir(&mat_b);
+  //mimprimir(&mat_b);
   // %%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%
 
   printf("\n%%%%%%%%%%%%%%%%\n");
 	
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
   //               Operações de Multiplicação
-  mmult = (mymatriz **) malloc (sizeof(mymatriz *));
-  printf("\n ##### multiplicar_t1 de Matrizes - MULTIPLICACAO <mmultiplicar> #####\n");
-  start_time = wtime();
-  mmult[0] = mmultiplicar(&mat_a, &mat_b, 1);
-  end_time = wtime();
-  mimprimir(mmult[0]);
-  tempos[0] = end_time - start_time;
-  printf("\tRuntime: %f\n", end_time - start_time);
-  sprintf(filename, "mult_t1.result");
-  fmat = fopen(filename,"w");
-  fileout_matriz(mmult[0], fmat);
-  fclose(fmat);
+  for(int i=0; i<10; i++) {
+    mmult = (mymatriz **) malloc (sizeof(mymatriz *));
+    printf("\n ##### multiplicar_t1 de Matrizes - MULTIPLICACAO <mmultiplicar> #####\n");
+    start_time = wtime();
+    mmult[0] = mmultiplicar(&mat_a, &mat_b, 1);
+    end_time = wtime();
+    //mimprimir(mmult[0]);
+    tempos[0][i] = end_time - start_time;
+    printf("\tRuntime: %f\n", end_time - start_time);
+    sprintf(filename, "mult_t1.result");
+    fmat = fopen(filename,"w");
+    fileout_matriz(mmult[0], fmat);
+    fclose(fmat);
+  }
   // %%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%
 
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
   //               Operações de Multiplicação (em bloco)
-  mmultbloco = (mymatriz **) malloc (sizeof(mymatriz *));
-  printf("\n ##### multiplicar_t1 de Matrizes - MULTIPLICACAO <por bloco> #####\n");
-  start_time = wtime();
+  for(int i=0; i<10; i++) {
+    mmultbloco = (mymatriz **) malloc (sizeof(mymatriz *));
+    printf("\n ##### multiplicar_t1 de Matrizes - MULTIPLICACAO <por bloco> #####\n");
+    start_time = wtime();
 
-  Vsubmat_a = particionar_matriz (mat_a.matriz, N, La, 1, 2);
-  Vsubmat_b = particionar_matriz (mat_b.matriz, Lb, M, 0, 2);
-  Vsubmat_c = csubmatrizv2 (N, M, nro_submatrizes);
+    Vsubmat_a = particionar_matriz (mat_a.matriz, N, La, 1, 2);
+    Vsubmat_b = particionar_matriz (mat_b.matriz, Lb, M, 0, 2);
+    Vsubmat_c = csubmatrizv2 (N, M, nro_submatrizes);
 
-  mmsubmatriz (Vsubmat_a[0], Vsubmat_b[0], Vsubmat_c[0]);
-  mmsubmatriz (Vsubmat_a[1], Vsubmat_b[1], Vsubmat_c[1]); 
-  mmultbloco[0] = msomar(Vsubmat_c[0]->matriz, Vsubmat_c[1]->matriz, 1);
+    mmsubmatriz (Vsubmat_a[0], Vsubmat_b[0], Vsubmat_c[0]);
+    mmsubmatriz (Vsubmat_a[1], Vsubmat_b[1], Vsubmat_c[1]); 
+    mmultbloco[0] = msomar(Vsubmat_c[0]->matriz, Vsubmat_c[1]->matriz, 1);
 	 
-  end_time = wtime();
-  printf("\n Matriz Resultado da AxB por bloco <mmultbloco> \n");
-  mimprimir(mmultbloco[0]);
-  tempos[1] = end_time - start_time;
-  printf("\tRuntime: %f\n", end_time - start_time);
-  sprintf(filename, "mult_t1.result");
-  fmat = fopen(filename,"w");
-  fileout_matriz(mmultbloco[0], fmat);
-  fclose(fmat);
+    end_time = wtime();
+    printf("\n Matriz Resultado da AxB por bloco <mmultbloco> \n");
+    //mimprimir(mmultbloco[0]);
+    tempos[1][i] = end_time - start_time;
+    printf("\tRuntime: %f\n", end_time - start_time);
+    sprintf(filename, "mult_t1.result");
+    fmat = fopen(filename,"w");
+    fileout_matriz(mmultbloco[0], fmat);
+    fclose(fmat);
+  }
   // %%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%
 
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
   //               Operações de Multiplicação thread
   mymatriz **mmult_thread = (mymatriz **) malloc (sizeof(mymatriz *));
-  printf("\n ##### multiplicar_t1 de Matrizes thread - MULTIPLICACAO <mmultiplicar> #####\n");
-  start_time = wtime();
-  mmult_thread[0] = multiplicar_omp(&mat_a, &mat_b, 1);
-  end_time = wtime();
-  mimprimir(mmult_thread[0]);
-  tempos[2] = end_time - start_time;
-  printf("\tRuntime: %f\n", end_time - start_time);
-  sprintf(filename, "mult_t1.result");
-  fmat = fopen(filename,"w");
-  fileout_matriz(mmult_thread[0], fmat);
-  fclose(fmat);
+  for(int i=0; i<10; i++) {
+    printf("\n ##### multiplicar_t1 de Matrizes thread - MULTIPLICACAO <mmultiplicar> #####\n");
+    start_time = wtime();
+    mmult_thread[0] = multiplicar_omp(&mat_a, &mat_b, 1);
+    end_time = wtime();
+    //mimprimir(mmult_thread[0]);
+    tempos[2][i] = end_time - start_time;
+    printf("\tRuntime: %f\n", end_time - start_time);
+    sprintf(filename, "mult_t1.result");
+    fmat = fopen(filename,"w");
+    fileout_matriz(mmult_thread[0], fmat);
+    fclose(fmat);
+  }
   // %%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%
 
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
   //               Operações de Multiplicação thread (em bloco)
   mymatriz **mmultbloco_thread = (mymatriz **) malloc (sizeof(mymatriz *));
-  printf("\n ##### multiplicar_t1 de Matrizes thread - MULTIPLICACAO <por bloco> #####\n");
-  start_time = wtime();
+  for(int i=0; i<10; i++) {
+    printf("\n ##### multiplicar_t1 de Matrizes thread - MULTIPLICACAO <por bloco> #####\n");
+    start_time = wtime();
 
-  Vsubmat_a = particionar_matriz (mat_a.matriz, N, La, 1, 2);
-  Vsubmat_b = particionar_matriz (mat_b.matriz, Lb, M, 0, 2);
-  Vsubmat_c = csubmatrizv2 (N, M, nro_submatrizes);
+    Vsubmat_a = particionar_matriz (mat_a.matriz, N, La, 1, 2);
+    Vsubmat_b = particionar_matriz (mat_b.matriz, Lb, M, 0, 2);
+    Vsubmat_c = csubmatrizv2 (N, M, nro_submatrizes);
 
-  mmsubmatriz_omp(Vsubmat_a[0], Vsubmat_b[0], Vsubmat_c[0]);
-  mmsubmatriz_omp(Vsubmat_a[1], Vsubmat_b[1], Vsubmat_c[1]); 
-  mmultbloco_thread[0] = msomar(Vsubmat_c[0]->matriz, Vsubmat_c[1]->matriz, 1);
+    mmsubmatriz_omp(Vsubmat_a[0], Vsubmat_b[0], Vsubmat_c[0]);
+    mmsubmatriz_omp(Vsubmat_a[1], Vsubmat_b[1], Vsubmat_c[1]); 
+    mmultbloco_thread[0] = msomar(Vsubmat_c[0]->matriz, Vsubmat_c[1]->matriz, 1);
 	 
-  end_time = wtime();
-  printf("\n Matriz Resultado da AxB por bloco <mmultbloco> \n");
-  mimprimir(mmultbloco_thread[0]);
-  tempos[3] = end_time - start_time;
-  printf("\tRuntime: %f\n", end_time - start_time);
-  sprintf(filename, "mult_t1.result");
-  fmat = fopen(filename,"w");
-  fileout_matriz(mmultbloco_thread[0], fmat);
-  fclose(fmat);
+    end_time = wtime();
+    printf("\n Matriz Resultado da AxB por bloco <mmultbloco> \n");
+    //mimprimir(mmultbloco_thread[0]);
+    tempos[3][i] = end_time - start_time;
+    printf("\tRuntime: %f\n", end_time - start_time);
+    sprintf(filename, "mult_t1.result");
+    fmat = fopen(filename,"w");
+    fileout_matriz(mmultbloco_thread[0], fmat);
+    fclose(fmat);
+  }
   // %%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%
 	
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
@@ -178,12 +187,12 @@ int main(int argc, char *argv[]) {
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
   //              Comparação dos resultados
 
-  printf("tempo médio matriz sequencial: %.6f\n", tempos[0]);
-  printf("tempo médio matriz sequencial bloco: %.6f\n", tempos[1]);
-  printf("tempo médio matriz paralela: %.6f\n", tempos[2]);
-  printf("tempo médio matriz paralela bloco: %.6f\n", tempos[3]);
-  imprime_speedup("speedup matriz:", tempos[0], tempos[2]);
-  imprime_speedup("speedup matriz bloco:", tempos[1], tempos[3]);
+  printf("tempo médio matriz sequencial: %.6f\n", soma_tempo(tempos[0]));
+  printf("tempo médio matriz sequencial bloco: %.6f\n", soma_tempo(tempos[1]));
+  printf("tempo médio matriz paralela: %.6f\n", soma_tempo(tempos[2]));
+  printf("tempo médio matriz paralela bloco: %.6f\n", soma_tempo(tempos[3]));
+  imprime_speedup("speedup matriz:", soma_tempo(tempos[0]), soma_tempo(tempos[2]));
+  imprime_speedup("speedup matriz bloco:", soma_tempo(tempos[1]), soma_tempo(tempos[3]));
   
   // %%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%
   // %%%%%%%%%%%%%%%%%%%%%%%% BEGIN %%%%%%%%%%%%%%%%%%%%%%%%
@@ -209,4 +218,13 @@ double speedup(double tempo_sequencial, double tempo_paralelo) {
 
 void imprime_speedup(char* descricao, double tempo_sequencial, double tempo_paralelo) {
   printf("%s: %f\n", descricao, speedup(tempo_sequencial, tempo_paralelo));
+}
+
+float soma_tempo(float tempo[10]){
+  float valor = 0;
+  for(int i=0; i<10; i++) {
+    valor += tempo[i];
+  }
+
+  return valor/10;
 }
